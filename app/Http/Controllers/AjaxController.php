e8da3af74d072411b0648dc69cea1283c5ef7609
rg4amia <r4gamia@gmail.com>
2019-06-26 01:00:23 +0000

    Management du site

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Model\Compte;
+use App\Model\Eleve;
 use Illuminate\Http\Request;
 
 class AjaxController extends Controller
 {
     public function compte_montant(Request $request){
 
-        $montant = Compte::where('id',$request->val)->get();
-        return response()->json($montant);
+            $compte = Compte::findOrfail($request->val);
+
+            $eleves = Eleve::mine()->whereHas('versement', function ($q) use ($request) {
+                return $q->where('eleve_id', $request->id_eleve );
+            })->with('versement')->get();
+
+            foreach ($eleves as $item){
+
+                $eleve[]=[
+                    'id' => $item->id,
+                    'nom' => $item->nom,
+                    'prenom' => $item->prenom,
+                ];
+
+                $data_montant = 0;
+
+                for ($y =0; $y < count($item->versement); $y++){
+                    $data_montant = $item->versement[$y]->montant + $data_montant;
+                    $eleve[0]['montantdverse'] = $data_montant;
+                    $eleve[0]['restepaye'] = $item->versement[$y]->resteapayer;
+                }
+
+            }
+
+            $eleve[0]['montantapayer']= $compte->montant;
+
+
+        return response()->json($eleve[0]);
+
     }
 }
--- resources/views/versement/create.blade.php
+++ resources/views/versement/create.blade.php
@@ -13,11 +13,12 @@
         <div class="card-content">
 
             <div class="card-body">
-                {{ Form::model($eleve, ['route'=>['versement.update',$eleve[0]['id']],'method' => 'PUT']) }}
+                {{ Form::model($eleve, ['route'=>['versement.update',$eleve[0]['id']],'method' => 'PUT', 'id' => 'form']) }}
                 <div class="row">
                     <div class="col-6">
                         <div class="form-group">
                             <label for="name">Nom & Prenom (Eleve)</label>
+                            <input type="hidden" name="id_eleve" value="{{$eleve[0]['id']}}" id="id_eleve">
                             {{ Form::text('nom', $eleve[0]['nom'].' '.$eleve[0]['prenom'], ['class'=>'form-control', 'required' => 'required']) }}
                         </div>
                     </div>
@@ -30,31 +31,36 @@
                     </div>
                     <div class="col-6">
                         <div class="form-group">
-                            <label for="name">Montant à Payer</label>
-                            <select class="form-control required" name="compte_montant" id="compte_montant">
-                            </select>
+                            <label for="name">Montant du Compte</label>
+                            {{ Form::text('montantapayer', null, ['id' => 'montantapayer','class'=>'form-control', 'required' => true, 'disable' => true]) }}
                         </div>
                     </div>
+
                     <div class="col-6">
                         <div class="form-group">
-                            <label for="name">Montant Versement</label>
-                            {{ Form::text('montant', null, ['id' => 'montant_vers','class'=>'form-control', 'disable' => 'true']) }}
-                        </div>
-                    </div>
-                   {{-- <button id="check" type="button"
-                            class="btn btn-icon btn-icon btn-relief-info mr-0 waves-effect waves-light">
-                            <i class="feather icon-link"></i>
-                            Versement
-                    </button>--}}
-                    <div class="col-6">
-                        <div class="form-group">
-                            <label for="name">Reste A Payer</label>
+                            <label for="name">Montant Déjà Versé</label>
                             <div id="reste_payer">
-                                {{ Form::text('montantdverse',$eleve[0]['montant'], ['id' => 'montantdvers','class'=>'form-control', 'disable' => 'true']) }}
+                                {{ Form::text('montantdverse',null, ['id' => 'montantdvers','class'=>'form-control', 'disable' => 'true']) }}
 
                             </div>
                         </div>
                     </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="name">Reste à Payer</label>
+                            {{ Form::number('montant', null, ['id' => 'restepaye','class'=>'form-control', 'required' => true ]) }}
+                        </div>
+                    </div>
+
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="name">Montant Versement</label>
+                            {{ Form::number('montant', null, ['id' => 'montant_vers','class'=>'form-control', 'required' => true ]) }}
+                            <span id="message"></span>
+
+                        </div>
+                    </div>
+
                 </div>
                 <button id="save" type="submit" class="btn btn-primary btn-icon-split mt-3">
                     <span class="icon text-white-50">
@@ -81,12 +87,12 @@
 
 @endsection
 @section('js')
-
-    <script>
+   {{-- <script>
         $(document).ready(function () {
             $("#montant_vers").on('keyup change',function() {
                 var val = $(this).val();
-                console.log(val);
+                var id_eleve = $("#id_eleve").val();
+
                 if(val != ''){
                     $("#reste_payer").empty();
                     $.ajaxSetup({
@@ -97,13 +103,14 @@
                     $.ajax({
                         url: "{{ route('get.montant') }}",
                         method: 'post',
-                        data:{val:val},
+                        data:{val:val,id_eleve:id_eleve},
                         dataType: 'json',
                         success: function(result){
                             //on rempli la seconde liste
                             $.each( result, function( key, value ) {
                                 console.log(value.id);
-                                $("#compte_montant").append('<input name="reste_payer" value="'+ value.montant+'">'+ value.montant +'</input>');
+                                $("#montantapayer").val(data.montant);
+                                $("#montantdverse").val(data.montantdverse);
                             });
                         },error:function(jqXHR, textStatus){
                             alert('Error.\n'+ jqXHR.responseText);
@@ -113,15 +120,46 @@
             });
         });
 
+    </script>--}}
+
+    <script>
+
+        $(document).ready(function (){
+
+            $("#montant_vers").on('keyup', function () {
+
+                var montant_vers = $(this).val();
+                var restepaye = $("#restepaye").val();
+                var message = $("#message");
+                console.log(restepaye - montant_vers);
+
+                /*if (montant_vers === restepaye){
+                    message.removeClass("badge-danger badge-info badge-success");
+                    message.html("Vous Voulez Solder le compte").addClass("badge-success");
+                }else if (montant_vers < restepaye){
+                    message.removeClass("badge-danger badge-info badge-success");
+                    message.html("Ce montant ne solde pas le Compte").addClass("badge-info");
+                }else if (montant_vers > restepaye) {
+                    message.removeClass("badge-danger badge-info badge-success");
+                    message.html("Attention ce montant est superieur au montant a verser").addClass("badge-danger");
+                    //$('#form').find('button[type="submit"]').attr('disabled', true);
+                }*/
+
+
+            })
+
+        });
+
     </script>
 
     <script>
         $(document).ready(function () {
             $("#compte").on('change',function() {
                 var val = $(this).val();
-                console.log(val);
-                if(val != ''){
-                    $("#compte_montant").empty();
+                var id_eleve = $("#id_eleve").val();
+                if(val > 0){
+                    $("#montantdvers").empty();
+                    $("#montantapayer").empty();
                     $.ajaxSetup({
                         headers: {
                             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -130,14 +168,13 @@
                     $.ajax({
                         url: "{{ route('get.montant') }}",
                         method: 'post',
-                        data:{val:val},
+                        data:{val:val,id_eleve:id_eleve},
                         dataType: 'json',
                         success: function(result){
-                            //on rempli la seconde liste
-                            $.each( result, function( key, value ) {
-                                console.log(value.id);
-                                $("#compte_montant").append('<option value="'+ value.montant+'">'+ value.montant +'</option>');
-                            });
+                            $("#montantdvers").val(result.montantdverse);
+                            $("#montantapayer").val(result.montantapayer);
+                            $("#restepaye").val(result.restepaye);
+
                         },error:function(jqXHR, textStatus){
                             alert('Error.\n'+ jqXHR.responseText);
                         }
